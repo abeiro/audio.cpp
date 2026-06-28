43a4c30596401e7dd715e5cbc1fd79b549e3edcd
abeiro <augusto.beiro@gmail.com>
2026-06-28 21:20:02 +0200

    Change size_t to std::size_t in noise.h
    Change size_t to std::size_t in noise.h, fill Debian compilation issue

--- include/engine/framework/sampling/noise.h
+++ include/engine/framework/sampling/noise.h
@@ -5,7 +5,7 @@
 
 namespace engine::sampling {
 
-std::vector<float> generate_normal_noise(size_t count, uint32_t seed, float scale = 1.0F);
+std::vector<float> generate_normal_noise(std::size_t count, uint32_t seed, float scale = 1.0F);
 void clamp_noise(std::vector<float> & noise, float min_value, float max_value);
 
 }  // namespace engine::sampling
